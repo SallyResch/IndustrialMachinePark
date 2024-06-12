1501c4a895af31fc8b9103e2c75f880c3d4e4422
Sillysally <92034603+SallyResch@users.noreply.github.com>
2024-06-12 09:16:41 +0200

    added seed data

--- IndustrialMachinePark/Data/AppDbContext.cs
+++ IndustrialMachinePark/Data/AppDbContext.cs
@@ -9,6 +9,18 @@ namespace IndustrialMachinePark.Data
         {
         }
         public DbSet<Machine> Machines { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Machine>().HasData(new Machine { Name = "Owen small", LatestData = "The machine is ready to start", IsOnline = false });
+            modelBuilder.Entity<Machine>().HasData(new Machine { Name = "Dough Maker", LatestData = "Waiting for ingredients", IsOnline = true });
+            modelBuilder.Entity<Machine>().HasData(new Machine { Name = "Rolling machine", LatestData = "Latest rolling: marsipan", IsOnline = false });
+            modelBuilder.Entity<Machine>().HasData(new Machine { Name = "Beater machine", LatestData = "Waiting for cream to enter the bowl", IsOnline = false });
+            modelBuilder.Entity<Machine>().HasData(new Machine { Name = "Owen Medium", LatestData = "The machine is ready to start", IsOnline = false });
+            modelBuilder.Entity<Machine>().HasData(new Machine { Name = "Owen Large", LatestData = "The machine is ready to start", IsOnline = false });
+        }
     }
     
 }
---
+++ IndustrialMachinePark/Migrations/20240612071408_seedDataMigration.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace IndustrialMachinePark.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedDataMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Machines",
+                columns: new[] { "Id", "IsOnline", "LatestData", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("1b90bf7b-ee60-4c03-8434-8968355a76a2"), true, "Waiting for ingredients", "Dough Maker" },
+                    { new Guid("3f939efe-71c2-4454-9f52-bac1efd85359"), false, "The machine is ready to start", "Owen Medium" },
+                    { new Guid("5ca63d37-73c8-4d91-bf82-e75fdfdc4f69"), false, "Latest rolling: marsipan", "Rolling machine" },
+                    { new Guid("78d1d32f-3342-4b6f-8937-fcd4942505ec"), false, "Waiting for cream to enter the bowl", "Beater machine" },
+                    { new Guid("a4d5881b-0ded-46de-93f8-4285e27cc6f2"), false, "The machine is ready to start", "Owen Large" },
+                    { new Guid("e69aa2f3-71d2-4823-8c44-c8189a4c3b1b"), false, "The machine is ready to start", "Owen small" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Machines",
+                keyColumn: "Id",
+                keyValue: new Guid("1b90bf7b-ee60-4c03-8434-8968355a76a2"));
+
+            migrationBuilder.DeleteData(
+                table: "Machines",
+                keyColumn: "Id",
+                keyValue: new Guid("3f939efe-71c2-4454-9f52-bac1efd85359"));
+
+            migrationBuilder.DeleteData(
+                table: "Machines",
+                keyColumn: "Id",
+                keyValue: new Guid("5ca63d37-73c8-4d91-bf82-e75fdfdc4f69"));
+
+            migrationBuilder.DeleteData(
+                table: "Machines",
+                keyColumn: "Id",
+                keyValue: new Guid("78d1d32f-3342-4b6f-8937-fcd4942505ec"));
+
+            migrationBuilder.DeleteData(
+                table: "Machines",
+                keyColumn: "Id",
+                keyValue: new Guid("a4d5881b-0ded-46de-93f8-4285e27cc6f2"));
+
+            migrationBuilder.DeleteData(
+                table: "Machines",
+                keyColumn: "Id",
+                keyValue: new Guid("e69aa2f3-71d2-4823-8c44-c8189a4c3b1b"));
+        }
+    }
+}
