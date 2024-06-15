d8f85a956b5e15ae401250f7b645ced51fee0d75
Sillysally <92034603+SallyResch@users.noreply.github.com>
2024-06-15 12:52:10 +0200

    added a page for adding a Machine

---
+++ IndustrialMachinePark/Components/Pages/AddMachine.razor.cs
@@ -0,0 +1,11 @@
+﻿using IndustrialMachinePark.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace IndustrialMachinePark.Components.Pages
+{
+    public partial class AddMachine
+    {
+        [SupplyParameterFromForm]
+        public Machine Machine { get; set; }
+    }
+}
