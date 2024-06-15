7790fa7fb560e7a98706c1bc88e6faa005fc2ce9
Sillysally <92034603+SallyResch@users.noreply.github.com>
2024-06-15 15:12:35 +0200

    added method contract to IMachineDataService

--- IndustrialMachinePark/Contracts/Services/IMachineDataService.cs
+++ IndustrialMachinePark/Contracts/Services/IMachineDataService.cs
@@ -6,6 +6,9 @@ namespace IndustrialMachinePark.Contracts.Services
     {
         Task<IEnumerable<Machine>> GetAllMachines();
         Task<Machine> GetMachineDetails (Guid Id);
+        Task<Machine> AddMachine (Machine machine);
+        Task<Machine> UpdateMachine (Machine machine);
+        Task<Machine> DeleteMachine (Guid Id);
 
 
         
